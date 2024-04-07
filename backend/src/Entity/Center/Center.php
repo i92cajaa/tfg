85c5e1c6ad3dfb3357dc8ce8b50ddac9244f4919
i92cajaa <i92cajaa@uco.es>
2024-04-07 16:40:18 +0200

    Center entity completed

--- backend/src/Entity/Center/Center.php
+++ backend/src/Entity/Center/Center.php
@@ -135,7 +135,125 @@ class Center
     // Setter Methods
     // ----------------------------------------------------------------
 
+    /**
+     * @param string $id
+     * @return $this
+     */
+    public function setId(string $id): Center
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @param Document $logo
+     * @return $this
+     */
+    public function setLogo(Document $logo): Center
+    {
+        $this->logo = $logo;
+        return $this;
+    }
+
+    /**
+     * @param Collection $users
+     * @return $this
+     */
+    public function setUsers(Collection $users): Center
+    {
+        $this->users = $users;
+        return $this;
+    }
+
+    /**
+     * @param Area $area
+     * @return $this
+     */
+    public function setArea(Area $area): Center
+    {
+        $this->area = $area;
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName(string $name): Center
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @param string $address
+     * @return $this
+     */
+    public function setAddress(string $address): Center
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * @param string $phone
+     * @return $this
+     */
+    public function setPhone(string $phone): Center
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
+    /**
+     * @param string $color
+     * @return $this
+     */
+    public function setColor(string $color): Center
+    {
+        $this->color = $color;
+        return $this;
+    }
+
     // ----------------------------------------------------------------
     // Other Methods
+    // ----------------------------------------------------------------
+
+    // ----------------------------------------------------------------
+    /**
+     * EN: FUNCTION TO ADD AN USER TO THIS CENTER
+     * ES: FUNCIÓN PARA AÑADIR UN USUARIO AL CENTRO
+     *
+     * @param User $user
+     * @return $this
+     */
+    // ----------------------------------------------------------------
+    public function addUser(User $user): Center
+    {
+        if (!$this->users->contains($user)) {
+            $this->users->add($user);
+        }
+
+        return $this;
+    }
+    // ----------------------------------------------------------------
+
+    // ----------------------------------------------------------------
+    /**
+     * EN: FUNCTION TO REMOVE AN USER FROM THIS CENTER
+     * ES: FUNCIÓN PARA BORRAR UN USUARIO DEL CENTRO
+     *
+     * @param User $user
+     * @return $this
+     */
+    // ----------------------------------------------------------------
+    public function removeUser(User $user): Center
+    {
+        if ($this->users->contains($user)) {
+            $this->users->removeElement($user);
+        }
+
+        return $this;
+    }
     // ----------------------------------------------------------------
 }
